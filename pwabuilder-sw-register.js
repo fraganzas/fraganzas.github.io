fd9c42be96b9e4b03eb3418703dc7f88002e74d3
Fragoso <joao.fragoso16@gmail.com>
2019-02-23 15:00:40 +0000

    Update pwabuilder-sw-register.js

--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -1,7 +1,7 @@
 //This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
 //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
-if (navigator.serviceWorker.controller) {
+if (navigator.serviceWorker) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
 
